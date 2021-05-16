8d5fe7f169aad66ed6cf1c2d477a81086d9cc891
liuxin <183771798@qq.com>
2021-05-16 13:57:19 +0800

    test

---
+++ src/main/java/com/algorithmanddatastructure/AgorithmanddatastructureApplication.java
@@ -0,0 +1,13 @@
+package com.algorithmanddatastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgorithmanddatastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgorithmanddatastructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/algorithmanddatastructure/AgorithmanddatastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithmanddatastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgorithmanddatastructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
